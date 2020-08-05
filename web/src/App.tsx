9e6db3753715b4bc539d2f48183c97399da2eac0
joandersonSJ <joandersonsantosdev@gmail.com>
2020-08-05 00:58:49 -0300

    :art: style: initial styling defined

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import { Global } from './assets/styles/global';
 
 const App: React.FC = () => {
   return (
     <>
-      <h1>Hello World</h1>;
+      <h1>Hello World</h1>
+      <Global />
     </>
   );
 };
